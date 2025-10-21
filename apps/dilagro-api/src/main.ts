a2d5683f7a2be5dc94f520495a8f8f332a3a59a3
Sathsara200 <tharidusathsara200@gmail.com>
2025-10-21 18:40:18 +0530

    bug fix

--- apps/dilagro-api/src/main.ts
+++ apps/dilagro-api/src/main.ts
@@ -9,9 +9,18 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ✅ Enable CORS for your Angular frontend
+  app.enableCors({
+    origin: 'http://localhost:4200', // Angular app URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
